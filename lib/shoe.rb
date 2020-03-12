40f5a1e12f0fecfcbbee9df7a38cd313d2bedce0
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-12 18:02:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,12 +5,12 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+     BRANDS << brand
   end
   
   def brand=(brand)
     @brand = brand
-    BRANDS << brand
-  
+   
   end
   
   def cobble
